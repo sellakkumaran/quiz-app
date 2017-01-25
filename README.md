460747f138393fe65e10690416c5e5daeb4de14a
Sellakkumaran S <sella-4744@sella-4744.csez.zohocorpin.com>
2017-01-25 14:37:22 +0530

    console log removed

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# quiz-app
+git# quiz-app
 
 This README outlines the details of collaborating on this Ember application.
 A short introduction of this app could easily go here.
--- app/router.js
+++ app/router.js
@@ -7,6 +7,9 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('welcome');
+  this.route('question',{path: '/question/:q_id'});
+  this.route('result');
 });
 
 export default Router;
